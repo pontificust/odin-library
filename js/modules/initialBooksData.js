cb53217826f4ca17946e6497df4970f20e241e4b
Pontificust <lihtinmann88@yandex.ru>
2026-03-01 14:21:51 +0300

    feat: add favicon and a few books'

--- js/modules/initialBooksData.js
+++ js/modules/initialBooksData.js
@@ -1,6 +1,12 @@
 export const initialBooksData = [
-    { title: "Crime and Punishment", author: "Fyodor Dostoevsky", pages: 530, isRead: true },
+    { title: "Crime and Punishment", author: "Fyodor Dostoevsky", pages: 530, isRead: false },
     { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 1200, isRead: false },
     { title: "The Gulag Archipelago", author: "Aleksandr Solzhenitsyn", pages: 1500, isRead: false },
-    { title: "Ward No. 6", author: "Anton Chekhov", pages: 150, isRead: true }
+    { title: "Ward No. 6", author: "Anton Chekhov", pages: 150, isRead: true },
+    { title: "How Much is a Human Worth?", author: "Evfrosiniya Kersnovskaya", pages: 800, isRead: true },
+    { title: "The Prince", author: "Niccolò Machiavelli", pages: 150, isRead: true },
+    { title: "Don Quixote", author: "Miguel de Cervantes", pages: 700, isRead: true },
+    { title: "Cabbages and Kings", author: "O. Henry", pages: 400, isRead: true },
+    { title: "The Bull", author: "Oleg Kashin", pages: 400, isRead: false },
+
 ];
